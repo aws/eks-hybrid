61d81bddaa14fc86222c53b6159ad8546d289a3f
Guillermo Gaston <guillermogastonlorente@gmail.com>
2025-03-05 12:08:36 -0600

    Revert "Fix bug on errors.IsType helper"

--- test/e2e/errors/type.go
+++ test/e2e/errors/type.go
@@ -3,6 +3,6 @@ package errors
 import "errors"
 
 // IsType returns true if any errors in the chain are of the specified type.
-func IsType[T error](err error, targetType T) bool {
+func IsType(err error, targetType interface{}) bool {
 	return errors.As(err, &targetType)
 }
