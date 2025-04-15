7db92947e327eb5627745f1f3796df90f124c015
Guillermo Gaston <gaslor@amazon.com>
2025-04-15 21:20:49 +0000

    Add third validation to verify kube-api server network and cert access

--- internal/node/hybrid/hybrid.go
+++ internal/node/hybrid/hybrid.go
@@ -25,9 +25,9 @@ const (
 	ntpSyncValidation     = "ntp-sync-validation"
 )
 
-// ValidationRunner is an interface for running validations.
-type ValidationRunner[O validation.Validatable[O]] interface {
-	Run(ctx context.Context, obj O, validations ...validation.Validation[O]) error
+// ValidationRunner runs validations.
+type ValidationRunner interface {
+	Run(ctx context.Context, obj *api.NodeConfig, validations ...validation.Validation[*api.NodeConfig]) error
 }
 
 type HybridNodeProvider struct {
@@ -44,8 +44,8 @@ type HybridNodeProvider struct {
 	certPath string
 	kubelet  Kubelet
 	// installRoot is optionally the root directory of the installation
-	installRoot  string
-	singleRunner ValidationRunner[*api.NodeConfig]
+	installRoot string
+	runner      ValidationRunner
 }
 
 type NodeProviderOpt func(*HybridNodeProvider)
@@ -63,7 +63,7 @@ func NewHybridNodeProvider(nodeConfig *api.NodeConfig, skipPhases []string, logg
 		network:    &defaultKubeletNetwork{},
 		certPath:   kubelet.KubeletCurrentCertPath,
 		kubelet:    kubelet.New(),
-		singleRunner: validation.NewSingleRunner[*api.NodeConfig](
+		runner: validation.NewSingleRunner[*api.NodeConfig](
 			validation.NewZapInformer(logger),
 			validation.WithSingleRunnerSkipValidations(skipValidations...),
 		),
@@ -114,10 +114,10 @@ func WithKubelet(kubelet Kubelet) NodeProviderOpt {
 	}
 }
 
-// WithSingleRunner sets the single runner for the HybridNodeProvider.
-func WithSingleRunner(runner ValidationRunner[*api.NodeConfig]) NodeProviderOpt {
+// WithValidationRunner sets the runner for the HybridNodeProvider.
+func WithValidationRunner(runner ValidationRunner) NodeProviderOpt {
 	return func(hnp *HybridNodeProvider) {
-		hnp.singleRunner = runner
+		hnp.runner = runner
 	}
 }
 
