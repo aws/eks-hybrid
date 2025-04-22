754cf9192c93b1240f59e887a7d1e460270f57e9
Vignesh Goutham Ganesh <vgg@amazon.com>
2025-04-22 14:39:59 -0500

    Remove pods with uninstall

--- internal/containerd/install.go
+++ internal/containerd/install.go
@@ -7,12 +7,17 @@ import (
 	"os/exec"
 	"time"
 
+	internalapi "github.com/containerd/containerd/integration/cri-api/pkg/apis"
+	"github.com/containerd/containerd/integration/remote"
 	"github.com/pkg/errors"
+	"go.uber.org/zap"
+	v1 "k8s.io/cri-api/pkg/apis/runtime/v1"
 
 	"github.com/aws/eks-hybrid/internal/artifact"
 	"github.com/aws/eks-hybrid/internal/daemon"
 	"github.com/aws/eks-hybrid/internal/system"
 	"github.com/aws/eks-hybrid/internal/tracker"
+	"github.com/aws/eks-hybrid/internal/util"
 	"github.com/aws/eks-hybrid/internal/util/cmd"
 )
 
@@ -126,4 +131,76 @@ func isContainerdNotInstalled() bool {
 	_, containerdNotFoundErr := exec.LookPath(containerdPackageName)
 	_, runcNotFoundErr := exec.LookPath(runcPackageName)
 	return containerdNotFoundErr != nil || runcNotFoundErr != nil
+}
+
+// Client is a containerd runtime client wrapper
+// Holds the internalapi.RuntimeService for pod/container operations
+type Client struct {
+	Runtime internalapi.RuntimeService
+}
+
+// NewClient creates a new Client with a real containerd runtime service
+func NewClient() (*Client, error) {
+	runtime, err := remote.NewRuntimeService(ContainerRuntimeEndpoint, 5*time.Second)
+	if err != nil {
+		return nil, err
+	}
+	return &Client{Runtime: runtime}, nil
+}
+
+func (c *Client) RemovePods() error {
+	podSandboxes, err := c.Runtime.ListPodSandbox(&v1.PodSandboxFilter{
+		State: &v1.PodSandboxStateValue{
+			State: v1.PodSandboxState_SANDBOX_READY,
+		},
+	})
+	if err != nil {
+		return err
+	}
+
+	for _, sandbox := range podSandboxes {
+		zap.L().Info("Stopping pod..", zap.String("pod", sandbox.Metadata.Name))
+		err := util.RetryExponentialBackoff(3, 2*time.Second, func() error {
+			if err := c.Runtime.StopPodSandbox(sandbox.Id); err != nil {
+				return err
+			}
+			if err := c.Runtime.RemovePodSandbox(sandbox.Id); err != nil {
+				return err
+			}
+			return nil
+		})
+		if err != nil {
+			zap.L().Info("ignored error stopping pod", zap.Error(err))
+		}
+	}
+
+	// If pod sandbox deletes dont work, we can try to stop and remove containers individually
+	containers, err := c.Runtime.ListContainers(nil)
+	if err != nil {
+		return errors.Wrap(err, "failed to list containers")
+	}
+
+	for _, container := range containers {
+		status, err := c.Runtime.ContainerStatus(container.Id)
+		if err != nil {
+			return errors.Wrapf(err, "failed to get container status for %s", container.Id)
+		}
+		zap.L().Info("Stopping container..", zap.String("container", container.Metadata.Name))
+		err = util.RetryExponentialBackoff(3, 2*time.Second, func() error {
+			if status.State == v1.ContainerState_CONTAINER_RUNNING {
+				if err := c.Runtime.StopContainer(container.Id, 0); err != nil {
+					return errors.Wrapf(err, "failed to stop container %s", container.Id)
+				}
+			}
+
+			if err := c.Runtime.RemoveContainer(container.Id); err != nil {
+				return errors.Wrapf(err, "failed to remove container %s", container.Id)
+			}
+			return nil
+		})
+		if err != nil {
+			zap.L().Info("ignored error removing container", zap.Error(err))
+		}
+	}
+	return nil
 }
